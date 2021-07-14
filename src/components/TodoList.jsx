944234f52f86a154a8f9e59f24951a13d3f64e9e
leeSangJun <molung@icloud.com>
2021-07-14 19:16:16 +0900

    투두리스트 구현 복습

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import styles from "./TodoList.module.css";
 import { BsTrash, BsCheck } from "react-icons/bs";
 
@@ -13,34 +13,91 @@ const {
   todoCheckBox,
 } = styles;
 
-const TodoItem = (props) => {
+const TodoItem = ({ onToggle, onDelete, content, id, checked }) => {
   return (
-    <div className={todoItem}>
-      <div className={todoCheckBox}>
-        <BsCheck />
+    <div className={todoItem} key={id}>
+      <div
+        className={todoCheckBox}
+        onClick={() => {
+          onToggle(id);
+        }}
+      >
+        {checked && <BsCheck />}
       </div>
-      <div className={todoContents}> 투두내용</div>
-      <div className={todoDelete}>
+      <div
+        className={todoContents}
+        onClick={() => {
+          onToggle(id);
+        }}
+      >
+        {" "}
+        {content}
+      </div>
+      <div
+        className={todoDelete}
+        onClick={() => {
+          onDelete(id);
+        }}
+      >
         <BsTrash />
       </div>
     </div>
   );
 };
 
-const TodoList = (props) => (
-  <div className={container}>
-    <div className={todoContainer}>
-      <div className={todoList}>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-      </div>
-      <div className={todoInput}>
-        <input />
-        <button>추가하기</button>
+const TodoList = (props) => {
+  //  const history = useHistory();
+
+  const [todos, setTodos] = useState([]);
+  const inputs = useRef();
+  const onCreate = (inputs) => {
+    setTodos(
+      todos.concat({
+        id: Date.now(),
+        content: inputs.current.value,
+        checked: false,
+      })
+    );
+    inputs.current.value = "";
+  };
+  const onToggle = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  };
+  const onDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className={container}>
+      <div className={todoContainer}>
+        <div className={todoList}>
+          {todos.map((todo) => (
+            <TodoItem
+              onToggle={onToggle}
+              onDelete={onDelete}
+              id={todo.id}
+              content={todo.content}
+              checked={todo.checked}
+            />
+          ))}
+        </div>
+        <div className={todoInput}>
+          <input ref={inputs} />
+          <button
+            onClick={() => {
+              onCreate(inputs);
+            }}
+          >
+            추가하기
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default TodoList;
